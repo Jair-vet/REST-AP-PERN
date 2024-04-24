5044fa7fd4fa4ea7beef39c6cce36509571134d8
Jair <jairaceves56@gmail.com>
2024-04-24 16:53:22 -0600

    Conexion a la base de Datos

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,27 @@
 import express  from 'express'
 import router from './router'
+import db from './config/db'
+
+
+// Conectar DB
+async function connectDB() {
+    try {
+        await db.authenticate()
+        db.sync()
+        console.log('Conexión exitosa a la BD');
+    } catch (error) {
+        console.log(error);
+        console.log('Hubo un error al conectar a la DB');
+        
+    }
+}
+
+connectDB()
 
 const server = express()
 
-
 server.use('/', router)
+
 
 
 export default server
