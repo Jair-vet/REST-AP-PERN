4e5901b65656cbc8db354c28aa73a362fe372a6b
Jair <jairaceves56@gmail.com>
2024-05-06 13:30:09 -0600

    Server done

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-console.log('Desde index.ts');
-console.log('Desde index.ts');
-(0, server_1.sumar)();
+const server_1 = __importDefault(require("./server"));
+const port = process.env.PORT || 4000;
+server_1.default.listen(port, () => {
+    console.log(`REST API en el puerto ${port}`);
+});
 //# sourceMappingURL=index.js.map
